63fb6016fd4bfe9c325feb95b50925e2a1086f98
SKKUKang <jun02727@g.skku.edu>
2024-11-20 20:08:05 +0900

    1

--- app/page.tsx
+++ app/page.tsx
@@ -162,8 +162,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-      <script src="script.js"></script>
+
 
       <form onSubmit={handleSubmit}>
         <input
